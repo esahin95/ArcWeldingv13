066e7bd20ac337e23f204e8791da519e290199e9
esahin95 <emre.sahin95@web.de>
2026-08-12 18:58:56 +0200

    working on new utility

--- src/functionObjects/traceSurface/traceSurface.C
+++ src/functionObjects/traceSurface/traceSurface.C
@@ -49,8 +49,6 @@ namespace functionObjects
 
 void Foam::functionObjects::traceSurface::writePositions()
 {
-    const vector gHat = n_;
-
     const volScalarField& alpha =
         mesh_.lookupObject<volScalarField>(alphaName_);
 
@@ -60,85 +58,41 @@ void Foam::functionObjects::traceSurface::writePositions()
             interpolation<scalar>::New(interpolationScheme_, alpha)
         );
 
-    if (Pstream::master())
+    const vector d0 = corners_[0];
+    const vector d1 = corners_[1] - corners_[0];
+    const vector d2 = corners_[2] - corners_[1];
+    const scalar dx = mag(d1) / scalar(nx_);
+    const scalar dy = mag(d2) / scalar(ny_);
+    const scalar dhx = dx / scalar(ns_);
+    const scalar dhy = dy / scalar(ns_);
+
+    scalar x = 0.0;
+    for (label i=0; i<nx_; i++)
     {
-        writeTime(file(fileID::heightFile));
-        writeTime(file(fileID::positionFile));
-    }
-
-    forAll(locations_, li)
-    {
-        // Create a set along a ray projected in the direction of gravity
-        const sampledSets::lineCellFace set
-        (
-            "",
-            mesh_,
-            coordSet::axisTypeNames_[coordSet::axisType::XYZ],
-            locations_[li] + gHat*mesh_.bounds().mag(),
-            locations_[li] - gHat*mesh_.bounds().mag()
-        );
-
-        Info<< set.faces() << endl;
-        Info<< set.cells() << endl;
-        Info<< set.segments() << endl;
-
-
-        // Find the height of the location above the boundary
-        scalar hLB =
-            set.size()
-          ? - gHat & (locations_[li] - set.coords().pointCoord(0))
-          : - vGreat;
-        reduce(hLB, maxOp<scalar>());
-
-        // Calculate the integrals of length and length*alpha along the sampling
-        // line. The latter is equal to the equivalent length with alpha equal
-        // to one.
-        scalar sumLength = 0, sumLengthAlpha = 0;
-        for (label si = 0; si < set.size() - 1; ++ si)
+        scalar y = 0.0;
+        for (label j=0; j<ny_; j++)
         {
-            if (set.segments()[si] != set.segments()[si+1]) continue;
 
-            const vector& p0 = set.coords().pointCoord(si);
-            const vector& p1 = set.coords().pointCoord(si+1);
-            const label c0 = set.cells()[si], c1 = set.cells()[si+1];
-            const label f0 = set.faces()[si], f1 = set.faces()[si+1];
 
-            if (f0 != -1 && f1 != -1) continue;
 
-            const scalar a0 = interpolator->interpolate(p0, c0, f0);
-            const scalar a1 = interpolator->interpolate(p1, c1, f1);
-
-            const scalar l = - gHat & (p1 - p0);
-            sumLength += l;
-            sumLengthAlpha += l*(a0 + a1)/2;
+            Info << x+0.5*dx << " " << y+0.5*dy << endl;
+            y += dy;
         }
 
-        reduce(sumLength, sumOp<scalar>());
-        reduce(sumLengthAlpha, sumOp<scalar>());
-
-        // Write out
-        if (Pstream::master())
-        {
-            // Interface heights above the boundary and location
-            const scalar hIB =
-                liquid_ ? sumLengthAlpha : sumLength - sumLengthAlpha;
-            const scalar hIL = hIB - hLB;
-
-            // Position of the interface
-            const point p = locations_[li] - gHat*hIL;
-
-            const Foam::Omanip<int> w = valueWidth(1);
-
-            file(fileID::heightFile) << w << hIB << w << hIL;
-            file(fileID::positionFile) << '(' << w << p.x() << w << p.y()
-                << valueWidth() << p.z() << ") ";
-        }
+        x += dx;
     }
+
 
     if (Pstream::master())
     {
-        file(fileID::heightFile).endl();
-        file(fileID::positionFile).endl();
+        writeTime(file());
+    }
+
+
+
+    if (Pstream::master())
+    {
+        file().endl();
     }
 }
 
@@ -147,68 +101,8 @@ void Foam::functionObjects::traceSurface::writePositions()
 
 void Foam::functionObjects::traceSurface::writeFileHeader(const label i)
 {
-    forAll(locations_, li)
-    {
-        writeHeaderValue
-        (
-            file(i),
-            "Location " + Foam::name(li),
-            locations_[li]
-        );
-    }
-
-    switch (fileID(i))
-    {
-        case fileID::heightFile:
-            writeHeaderValue
-            (
-                file(i),
-                "hB",
-                "Interface height above the boundary"
-            );
-            writeHeaderValue
-            (
-                file(i),
-                "hL",
-                "Interface height above the location"
-            );
-            break;
-        case fileID::positionFile:
-            writeHeaderValue(file(i), "p", "Interface position");
-            break;
-    }
-
-    const Foam::Omanip<int> w = valueWidth(1);
-
-    writeCommented(file(i), "Location");
-    forAll(locations_, li)
-    {
-        switch (fileID(i))
-        {
-            case fileID::heightFile:
-                file(i) << w << li << w << ' ';
-                break;
-            case fileID::positionFile:
-                file(i) << w << li << w << ' ' << w << ' ' << "  ";
-                break;
-        }
-    }
-    file(i).endl();
-
-    writeCommented(file(i), "Time");
-    forAll(locations_, li)
-    {
-        switch (fileID(i))
-        {
-            case fileID::heightFile:
-                file(i) << w << "hB" << w << "hL";
-                break;
-            case fileID::positionFile:
-                file(i) << w << "p" << w << ' ' << w << ' ' << "  ";
-                break;
-        }
-    }
-    file(i).endl();
+    writeCommented(file(), "Location");
+    file().endl();
 }
 
 
@@ -223,13 +117,18 @@ Foam::functionObjects::traceSurface::traceSurface
 :
     fvMeshFunctionObject(name, runTime, dict),
     logFiles(mesh_, name),
-    alphaName_("alpha"),
-    liquid_(true),
-    locations_(),
-    interpolationScheme_("cellPoint"),
-    n_(0,0,-1)
+    alphaName_(dict.lookup("alpha")),
+    corners_(dict.lookup("corners")),
+    nx_(dict.lookup<label>("nx")),
+    ny_(dict.lookup<label>("ny")),
+    ns_(dict.lookup<label>("ns")),
+    interpolationScheme_(
+        dict.lookupOrDefault<word>("interpolationScheme", "cellPoint")
+    )
 {
     read(dict);
+
+    resetName(typeName);
 }
 
 
@@ -240,20 +139,6 @@ Foam::functionObjects::traceSurface::~traceSurface()
 
 
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
-
-bool Foam::functionObjects::traceSurface::read(const dictionary& dict)
-{
-    fvMeshFunctionObject::read(dict);
-
-    dict.readIfPresent("alpha", alphaName_);
-    dict.readIfPresent("liquid", liquid_);
-    dict.lookup("locations") >> locations_;
-    dict.readIfPresent("interpolationScheme", interpolationScheme_);
-
-    resetNames({"height", "position"});
-
-    return true;
-}
 
 
 Foam::wordList Foam::functionObjects::traceSurface::fields() const
